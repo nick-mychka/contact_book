adce5d5ca6b52a10ae2ec919488f5dde1d828f49
Nick Mychka <nickmychka@gmail.com>
2018-03-17 23:24:13 +0200

    added the transparent overlay to close forms

--- js/script.js
+++ js/script.js
@@ -23,7 +23,8 @@ $(document).ready(function() {
         $(".btn_remove, .btn_edit").removeClass("hidden_content");
     });
 
-    var formAdd = $(".ls_form_add"),
+    var formOverlay = $(".overley"),
+        formAdd = $(".ls_form_add"),
         formEdit = $(".ls_form_edit"),
         formDelete = $(".ls_form_delete"),
         formNewGroup = $(".ls_form_add_group");
@@ -32,12 +33,14 @@ $(document).ready(function() {
         formAdd.toggleClass("is_visible");
         formEdit.removeClass("is_visible");
         formEdit[0].reset();
+        formOverlay.show(700);
     });
 
     $(".btn_edit").on("click", function(){
         formEdit.toggleClass("is_visible");
         formAdd.removeClass("is_visible");
         formAdd[0].reset();
+       formOverlay.show(700);
 
         var takeName = $(".active").data("name"), 
             takeEmail = $(".active").data("email"), 
@@ -51,16 +54,29 @@ $(document).ready(function() {
     });
 
     $(".btn_remove").on("click", function(){
+        formOverlay.show(700);
         formDelete.toggleClass('is_visible');
         var deleteContact = $(".active").data("name");
         $(".warn_q").text(deleteContact);
     });
 
+    $(".overley").on("click", function() {
+        formOverlay.hide(500);
+        formAdd.removeClass("is_visible");
+        formEdit.removeClass("is_visible");
+        formDelete.removeClass("is_visible");
+        formNewGroup.removeClass("is_visible");
+        formAdd[0].reset();
+        formEdit[0].reset();
+    });
+
     $(".cancel_delete").on("click", function() {
+        formOverlay.hide(500);
         formDelete.removeClass("is_visible");
     });
 
     $(".confirm_delete").on("click", function() {
+        formOverlay.hide(500);
         $(".active").remove();
         catchInform('', '', '', '');
         formDelete.removeClass("is_visible");
@@ -68,6 +84,7 @@ $(document).ready(function() {
     });
 
     $(".close_btn").on("click", function() {
+        formOverlay.hide(500);
         formAdd.removeClass("is_visible");
         formEdit.removeClass("is_visible");
         formDelete.removeClass("is_visible");
@@ -92,6 +109,7 @@ $(document).ready(function() {
                                         + pullName
                                     +'<i class="fa fa-exchange transfer" aria-hidden="true"></i></li>');
             formAdd.removeClass("is_visible");
+            formOverlay.hide(500);
             formAdd[0].reset();
         } else {
             alert("Please, enter the name of your contact!");
@@ -113,10 +131,12 @@ $(document).ready(function() {
         $(".active").text(changeName);
 
         formEdit.removeClass("is_visible");
+        formOverlay.hide(500);
         formEdit[0].reset();
     });
 
     $(".add_newgroup").on("click", function() {
+        formOverlay.show(800);
         formNewGroup.toggleClass("is_visible");
     });
 
@@ -127,6 +147,7 @@ $(document).ready(function() {
             $(".edit_group").append('<option>' + pullGroupName + '</option>');
             $(".add_group").append('<option>' + pullGroupName + '</option>');
             formNewGroup.removeClass("is_visible");
+            formOverlay.hide(500);
             formNewGroup[0].reset();
         } else {
             alert("Please, enter the name of your group!");
